27d7e9ad8e88ca4a113617e8ec5d55fa4526d8af
Wryhder <jemimahsummers@yahoo.com>
2020-01-13 12:51:46 +0100

    Add routes for Home and GameScreen views

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,23 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Home from "./views/Home.vue";
+import GameScreen from "./views/GameScreen.vue";
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/game-screen",
+      name: "Game Screen",
+      component: GameScreen
+    },
+  ]
 });
