d018cb1149865dec94ce786f66dc8ee1aa8ad30b
Wryhder <jemimahsummers@yahoo.com>
2020-01-13 12:52:03 +0100

    Create initial state

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,11 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    numberOfButtonsForCurrentLevel: 4,
+    playerName: "",
+    sessionScores: []
+  },
   mutations: {},
   actions: {},
   modules: {}
