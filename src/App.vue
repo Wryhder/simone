996c2778ad3064d4f41e91e986857f9faac4527e
Wryhder <jemimahsummers@yahoo.com>
2020-02-08 19:06:15 +0100

    Style Home view (PS: I'm lovin' the new look!)

--- src/App.vue
+++ src/App.vue
@@ -24,20 +24,12 @@ export default class App extends Vue {
 
 <style>
 #app {
+  height: inherit;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-
-.buttons {
-  margin-top: 6em;
-}
-
-.buttons button {
-  padding: 10px;
-  margin-right: 20px;
+  /* margin-top: 60px; */
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
+  <div id="home">
     <h1>Simone</h1>
     <p>
       Welcome to the Sybil System grading portal! <br />
-      Find out your criminal coefficient with fun games. <br />
-      Your final grade will be calculated as the overall average score of all
-      game scores recorded.
+      Find out your criminal coefficient with fun games. Your final score will be calculated as the overall average of all scores recorded.
     </p>
     <div class="buttons">
       <button @click="startGame" :to="{ name: 'Game Screen' }">
         Start New Game
       </button>
-      <button @click="resumeGame">Resume Saved Game</button>
+      <button @click="resumeGame">Resume Saved Game<span style=" vertical-align: super;">coming soon!</span></button>
     </div>
   </div>
 </template>
@@ -29,4 +27,52 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ @import url('https://fonts.googleapis.com/css?family=Audiowide&display=swap');
+
+#home {
+  height: 100%;
+  width: 100%;
+  background-image: url('../assets/psycho-pass-full-1447619_homepage.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: chocolate;
+  background-blend-mode: color-burn;
+}
+
+h1 {
+  margin-top: 0;
+  padding-top: 1.5em;
+  color: white;
+  font-family: 'Audiowide', cursive;
+}
+
+p {
+  margin-top: 10em;
+  width: 60%;
+  margin: 5em auto;
+  font-size: 1.5em;
+  color: white;
+  background-color: black;
+  padding: 1.5em;
+  border-radius: 10px;
+}
+
+.buttons {
+  margin-top: 6em;
+}
+
+.buttons button {
+  padding: 10px;
+  margin-right: 60px;
+  color: black;
+  background-color: navajowhite;
+  font-size: 1em;
+}
+
+button span {
+  vertical-align: super;
+  font-size: 11px;
+  color: purple;
+}
+</style>
