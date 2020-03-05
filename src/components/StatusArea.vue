faaa9e18631da270b968986a4f99285867ce8bae
Wryhder <jemimahsummers@yahoo.com>
2020-03-05 16:46:02 +0100

    Prettier formatting

--- src/components/StatusArea.vue
+++ src/components/StatusArea.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="statusarea">
-      {score}
+    {score}
   </div>
 </template>
 
 <script>
 export default {
-    props: ["score"]
-}
+  props: ["score"]
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
